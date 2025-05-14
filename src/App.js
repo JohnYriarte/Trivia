7c1e65af6c8bdd840373db1ee9607577106c881f
John Benedict Yriarte <johnbenedictyriarte0303@gmail.com>
2025-05-14 13:29:30 +0200

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './styles/App.css'
-import Button from './components/Button/Button'
 import { useState } from 'react'
 import StartPage from './components/StartPage/StartPage'
 import Quiz from './containers/Quiz/Quiz'
 
 function App() {
-  const [difficulty, setDifficulty] = useState(null);
   const [questions, setQuestions] = useState([]);
-
-  const onReset = () => {
-    setDifficulty(null);
-    setQuestions([]);
-  }
   
   const getQuestions = (difficulty) => {
     fetch(`https://opentdb.com/api.php?amount=10&difficulty=${difficulty}`)
     .then(res => res.json())
     .then(data => {
       setQuestions(data.results)
-      console.log(data.results)
     })
   }
 
